f1a97733113479d4f9f71baa81c9a14d4a111790
dbzdivik <dbz.divik@gmail.com>
2018-09-10 15:32:15 +0530

    Test 1

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,23 +1,18 @@
-#
-# Copyright (C) 2018 The TwrpBuilder Open-Source Project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-
 LOCAL_PATH := device/rockchip/rk3288
 
 TARGET_BOARD_PLATFORM := rk3288
 TARGET_BOOTLOADER_BOARD_NAME := rk3288
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_CPU_VARIANT := cortex-a7
+TARGET_CPU_SMP := true
+TARGET_BOARD_PLATFORM_GPU := mali-t764
+
+# Sensors
+BOARD_SENSOR_ST := false
+BOARD_SENSOR_MPU := true
+BOARD_USES_GENERIC_INVENSENSE := false
 
 # Recovery
 TARGET_USERIMAGES_USE_EXT4 := true
@@ -33,4 +28,3 @@ TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_INCLUDE_CRYPTO := true
 include $(LOCAL_PATH)/kernel.mk
 include device/generic/twrpbuilder/BoardConfig32.mk
-
